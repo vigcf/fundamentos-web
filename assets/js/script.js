46a58514ff5a7dcb043d60b6d09152f5d9074b2b
VitoriaAlric <vitoriacf@hotmail.com>
2021-01-13 14:54:38 -0300

    Config with JS

---
+++ assets/js/script.js
@@ -0,0 +1,74 @@
+/*
+Case sensitive: Reconhece letras maisculas e minusculas
+Meios de acesso ao Dow:
+Por Tag: getElementByTagName()
+Por iD: getElementById
+Por nome: getElementsByName
+Por classe: getElementsByIdByClassName()
+Por Seletor: querySelector()
+*/
+//variaveis
+let nome = window.document.getElementById('nome')
+let email = document.querySelector('#email')
+let area = document.querySelector('#assunto')
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
+let mapa = document.querySelector('#mapa')
+
+nome.style.width = '100%'
+email.style.width = '100%'
+assunto.style.width = '100%'
+
+function validaNome() {
+    let txtNome = document.querySelector('#txtNome')
+
+    if (nome.value.length < 3) {
+        txtNome.innerHTML = 'Nome inválido'
+        txtNome.style.color = 'red'
+    } else {
+        txtNome.innerHTML = 'Nome válido'
+        txtNome.style.color = 'green'
+        nomeOk = true
+
+    }
+}
+function validaEmail() {
+    let txtEmail = document.querySelector('#txtEmail')
+
+    if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1) {
+        txtEmail.innerHTML = 'Email inválido'
+        txtEmail.style.color = 'red'
+    } else {
+        txtEmail.innerHTML = 'Email válido'
+        txtEmail.style.color = 'green'
+        emailOk = true
+    }
+}
+function validaAssunto() {
+    let txtAssunto = document.querySelector('#txtAssunto')
+
+    if (assunto.value.length >= 100) {
+        txtAssunto.innerHTML = 'Quantidade de palavras excede o valor permitido, digite no máximo 100 caracteres!'
+        txtAssunto.style.color = 'red'
+        txtAssunto.style.display = 'block'
+    } else {
+        txtAssunto.style.display = 'none'
+        assuntoOk = true
+    }
+}
+function enviar() {
+    if (nomeOk == true && emailOk == true && assuntoOk == true) {
+        alert('Formulário enviado com sucesso!')
+    } else {
+        alert('Preencha o formulário corretamente antes de enviar...')
+    }
+} 
+function mapaZoom() {
+    mapa.style.width ='800px'
+    mapa.style.height ='600px'
+    }
+function mapaNormal() {
+    mapa.style.width ='400px'
+    mapa.style.height ='250px'
+    }
